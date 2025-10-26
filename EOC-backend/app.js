e1835aa511b2d148bf56bff032f580f4e49b57a1
charan <charanchilukuri2299@gmail.com>
2025-10-26 11:24:39 +0530

    added

--- EOC-backend/app.js
+++ EOC-backend/app.js
@@ -3,11 +3,6 @@ import express from 'express';
 import mongoose from 'mongoose';
 import cors from 'cors';
 
-const corsOptions = {
-  origin: 'https://eoc-website.vercel.app',
-  credentials: true,
-};
-
 
 
 import connectDB from './database/mongodb.js';
@@ -24,7 +19,8 @@ import errorMiddleware from './middlewares/error.js';
 
 const app = express()
 
-app.use(cors(corsOptions));
+app.use(cors({ origin: "https://eoc-website.vercel.app" }));
+
 app.get('/',(req,res)=>{
     res.status(200).json({
         'data':'Welcome to EOC backend routes'
@@ -43,7 +39,7 @@ app.use(errorMiddleware);
 
 
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 connectDB();
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);

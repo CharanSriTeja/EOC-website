a3edf4e482609e71153c16f4a1d0bb5416b71e05
charan <charanchilukuri2299@gmail.com>
2025-10-26 11:42:53 +0530

    added

--- EOC-backend/app.js
+++ EOC-backend/app.js
@@ -18,7 +18,7 @@ import errorMiddleware from './middlewares/error.js';
 
 
 const app = express()
-
+app.use(express.json());
 app.use(cors({ origin: "https://eoc-website.vercel.app" }));
 
 app.get('/',(req,res)=>{

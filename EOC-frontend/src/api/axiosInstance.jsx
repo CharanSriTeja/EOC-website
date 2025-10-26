4152915061d723b4242da7991f46bc137465ca8a
charan <charanchilukuri2299@gmail.com>
2025-10-26 10:39:17 +0530

    changes

--- EOC-frontend/src/api/axiosInstance.jsx
+++ EOC-frontend/src/api/axiosInstance.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:5000/api/v1', // Update to your backend URL
+  baseURL: 'https://eoc-website.vercel.app/api/v1', // Update to your backend URL
   headers: {
     'Content-Type': 'application/json',
   },
